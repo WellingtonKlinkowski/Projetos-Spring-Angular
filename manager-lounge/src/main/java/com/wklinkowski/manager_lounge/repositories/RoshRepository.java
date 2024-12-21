2df090b9723fd751efa0070aad9827197c49bd19
WellingtonKlinkowski <wellimgtomklim@gmail.com>
2024-12-21 12:32:55 -0300

    Criando consultas para a entidade Rosh

--- manager-lounge/src/main/java/com/wklinkowski/manager_lounge/repositories/RoshRepository.java
+++ manager-lounge/src/main/java/com/wklinkowski/manager_lounge/repositories/RoshRepository.java
@@ -1,9 +1,29 @@
 package com.wklinkowski.manager_lounge.repositories;
 
 import com.wklinkowski.manager_lounge.entities.RoshEntity;
+import com.wklinkowski.manager_lounge.enums.MarcasNarguile;
+import com.wklinkowski.manager_lounge.enums.MarcasRosh;
+import com.wklinkowski.manager_lounge.enums.MaterialRosh;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface RoshRepository extends JpaRepository<RoshEntity, Long> {
+
+    List<RoshEntity> findByMarcasRoshOrderByMarcasRoshDesc (MarcasNarguile marcasNarguile);
+
+    List<RoshEntity> findByMaterialRoshOrderByMaterialRoshDesc (MaterialRosh materialRosh);
+
+    @Query(value = "SELECT * FROM ROSHS r WHERE r.marcas_rosh LIKE %?1%", nativeQuery = true)
+    List<RoshEntity> procuraMarcasRoshComMetodoLike (String marcasNarguile);
+
+    @Query(value = "SELECT * FROM ROSHS r WHERE r.material_rosh LIKE %?1%", nativeQuery = true)
+    List<RoshEntity> procuraMaterialRoshComMetodoLike (String materialRosh);
+
+    void deleteByMarcasRosh (MarcasRosh marcasRosh);
+
+    void deleteByMaterialRosh (MaterialRosh materialRosh);
 }
