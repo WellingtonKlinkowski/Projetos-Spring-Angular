0412a9ffb34f8de6b2d344673f8e60b0c07963cd
WellingtonKlinkowski <wellimgtomklim@gmail.com>
2024-12-21 12:19:10 -0300

    Criando querys de consulta para a entidade Fumo

--- manager-lounge/src/main/java/com/wklinkowski/manager_lounge/repositories/FumoRepository.java
+++ manager-lounge/src/main/java/com/wklinkowski/manager_lounge/repositories/FumoRepository.java
@@ -1,10 +1,29 @@
 package com.wklinkowski.manager_lounge.repositories;
 
 import com.wklinkowski.manager_lounge.entities.FumoEntity;
+import com.wklinkowski.manager_lounge.enums.MarcasFumo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface FumoRepository extends JpaRepository<FumoEntity, Long> {
 
+    List<FumoEntity> findByMarcasFumoOrderByMarcasFumoDesc (MarcasFumo marcasFumo);
+
+    List<FumoEntity> findBySaborFumoOrderBySaborFumoDesc (String saborFumo);
+
+    List<FumoEntity> findByPesoFumoOrderByPesoFumoDesc (Integer pesoFumo);
+
+    @Query(value = "SELECT * FROM FUMOS f WHERE f.marcas_fumo LIKE %?1%", nativeQuery = true)
+    List<FumoEntity> procuraMarcasFumoComMetodoLike (String marcasFumo);
+
+    @Query(value = "SELECT * FROM FUMOS f WHERE f.sabor_fumo LIKE %?1%")
+    List<FumoEntity> procuraSaborFumoComMetodoLike (String saborFumo);
+
+    List<FumoEntity> findByPesoFumoBetween (Integer pesoMinimo, Integer pesoMaximo);
+
+    void deleteByMarcasFumo(MarcasFumo marcasFumo);
 }
