d03ba964c5ca60394c9367ae4c87405706d1f31f
WellingtonKlinkowski <wellimgtomklim@gmail.com>
2024-12-21 12:55:37 -0300

    Criando métodos de consultas e delete no service da entidade Narguile

--- manager-lounge/src/main/java/com/wklinkowski/manager_lounge/services/NarguileService.java
+++ manager-lounge/src/main/java/com/wklinkowski/manager_lounge/services/NarguileService.java
@@ -2,6 +2,8 @@ package com.wklinkowski.manager_lounge.services;
 
 import com.wklinkowski.manager_lounge.dtos.NarguileDTO;
 import com.wklinkowski.manager_lounge.entities.NarguileEntity;
+import com.wklinkowski.manager_lounge.enums.MarcasNarguile;
+import com.wklinkowski.manager_lounge.enums.MaterialNarguile;
 import com.wklinkowski.manager_lounge.exceptions.EntidadeNaoEncontrada;
 import com.wklinkowski.manager_lounge.repositories.NarguileRepository;
 import jakarta.transaction.Transactional;
@@ -64,4 +66,90 @@ public class NarguileService {
         narguileRepository.delete(narguileEntity);
     }
 
+    @Transactional
+    public List<NarguileDTO> procuraNarguilePorNomeNarguile (String nomeNarguile) {
+        List<NarguileEntity> listaNarguilePorNome =
+                narguileRepository.findByNomeNarguileOrderByNomeNarguileDesc(nomeNarguile);
+
+        return listaNarguilePorNome.stream().map(narguile ->
+                new NarguileDTO(narguile)).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public List<NarguileDTO> procuraNarguilePorMarcaNarguile (MarcasNarguile marcaNarguile) {
+        List<NarguileEntity> listaNarguilePorMarca =
+                narguileRepository.findByMarcasNarguileOrderByMarcasNarguileDesc(marcaNarguile);
+
+        return listaNarguilePorMarca.stream().map(narguile ->
+                new NarguileDTO(narguile)).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public List<NarguileDTO> procuraNarguilePorQuantidadeMangueirasNarguile (Integer quantidadeMangueirasNarguile) {
+        List<NarguileEntity> listaNarguilePorQuantidadeMangueiras =
+                narguileRepository.findByQuantidadeMangueirasNarguileOrderByQuantidadeMangueirasNarguileDesc(quantidadeMangueirasNarguile);
+
+        return listaNarguilePorQuantidadeMangueiras.stream().map(narguile ->
+                new NarguileDTO(narguile)).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public List<NarguileDTO> procuraNarguilePorMaterialNarguile (MaterialNarguile materialNarguile) {
+        List<NarguileEntity> listaNarguilePorMaterial =
+                narguileRepository.findByMaterialNarguileOrderByMaterialNarguileDesc(materialNarguile);
+
+        return listaNarguilePorMaterial.stream().map(narguile ->
+                new NarguileDTO(narguile)).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public List<NarguileDTO> procuraNarguilePorNomeNarguileComMetodoLike (String nomeNarguile) {
+        List<NarguileEntity> listaNarguilePorMaterial =
+                narguileRepository.procuraNomeNarguileComMetodoLike(nomeNarguile);
+
+        return listaNarguilePorMaterial.stream().map(narguile ->
+                new NarguileDTO(narguile)).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public List<NarguileDTO> procuraNarguilePorMarcasNarguileComMetodoLike (String marcaNarguile) {
+        List<NarguileEntity> listaNarguilePorMarca =
+                narguileRepository.procuraMarcasNarguileComMetodoLike(marcaNarguile);
+
+        return listaNarguilePorMarca.stream().map(narguile ->
+                new NarguileDTO(narguile)).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public List<NarguileDTO> procuraNarguilePorMaterialNarguileComMetodoLike (String materialNarguile) {
+        List<NarguileEntity> listaNarguilePorMaterial =
+                narguileRepository.procuraMaterialNarguileComMetodoLike(materialNarguile);
+
+        return listaNarguilePorMaterial.stream().map(narguile ->
+                new NarguileDTO(narguile)).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public List<NarguileDTO> procuraNarguileEntreQuantidadeMangueirasNarguile (Integer quantidadeMangueirasMinimo, Integer quantidadeMangueirasMaximo) {
+        List<NarguileEntity> listaNarguilePorQuantidadeMangueiras =
+                narguileRepository.findByQuantidadeMangueirasNarguileBetwenn(quantidadeMangueirasMinimo, quantidadeMangueirasMaximo);
+
+        return listaNarguilePorQuantidadeMangueiras.stream().map(narguile ->
+                new NarguileDTO(narguile)).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void apagaNarguilePorMarcasNarguile (MarcasNarguile marcasNarguile) {
+        narguileRepository.deleteByMarcasNarguile(marcasNarguile);
+    }
+
+    @Transactional
+    public void apagaNarguilePorMaterialNarguile (MaterialNarguile materialNarguile) {
+        narguileRepository.deleteByMaterialNarguile(materialNarguile);
+    }
+
+    @Transactional
+    public void apagaNarguilePorQuantidadeMangueirasNarguile (Integer quantidadeMangueirasNarguile) {
+        narguileRepository.deleteByQuantidadeMangueirasNarguile(quantidadeMangueirasNarguile);
+    }
 }
