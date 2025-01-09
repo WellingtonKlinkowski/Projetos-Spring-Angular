7acd3a812be7c9eb9d5de0c9ce12f947fe8fc85f
WellingtonKlinkowski <wellimgtomklim@gmail.com>
2025-01-09 09:59:44 -0300

    Adicionando atríbuto no DTO de Rosh

--- manager-lounge/src/main/java/com/wklinkowski/manager_lounge/dtos/RoshDTO.java
+++ manager-lounge/src/main/java/com/wklinkowski/manager_lounge/dtos/RoshDTO.java
@@ -5,6 +5,7 @@ import com.wklinkowski.manager_lounge.enums.MarcasRosh;
 import com.wklinkowski.manager_lounge.enums.MaterialRosh;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.springframework.beans.BeanUtils;
@@ -24,6 +25,9 @@ public class RoshDTO {
     @NotNull(message = "O material do rosh não pode estar vazio.")
     @Enumerated(EnumType.STRING)
     private MaterialRosh materialRosh;
+
+    @Min(value = 1, message = "A quantidade de rosh deve ser igual a 1.")
+    private Integer quantidadeEstoqueRosh = 1;
 
     public RoshDTO (RoshEntity roshEntity){
         BeanUtils.copyProperties(roshEntity, this);
