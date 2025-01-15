5b09120292f655f5590ea6d90d39ca9a3479a566
WellingtonKlinkowski <wellimgtomklim@gmail.com>
2025-01-15 10:57:01 -0300

    Aproveitei para criar o controller de aluguel, porém sem os endpoints por enquanto

---
+++ manager-lounge/src/main/java/com/wklinkowski/manager_lounge/controller/AluguelController.java
@@ -0,0 +1,21 @@
+package com.wklinkowski.manager_lounge.controller;
+
+import com.wklinkowski.manager_lounge.services.AluguelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/aluguel")
+public class AluguelController {
+
+    private final AluguelService aluguelService;
+
+    @Autowired
+    public AluguelController (AluguelService aluguelService){
+        this.aluguelService = aluguelService;
+    }
+
+    
+
+}
