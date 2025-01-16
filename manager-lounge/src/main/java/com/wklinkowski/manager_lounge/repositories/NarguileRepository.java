5cb4229d3b6d5024fbcd6830e7f86aeb1aa44664
WellingtonKlinkowski <wellimgtomklim@gmail.com>
2025-01-16 10:29:14 -0300

    Ajustes no respository de Narguile

--- manager-lounge/src/main/java/com/wklinkowski/manager_lounge/repositories/NarguileRepository.java
+++ manager-lounge/src/main/java/com/wklinkowski/manager_lounge/repositories/NarguileRepository.java
@@ -12,30 +12,30 @@ import java.util.List;
 @Repository
 public interface NarguileRepository extends JpaRepository<NarguileEntity, Long> {
 
-    List<NarguileEntity> findByNomeNarguileOrderByNomeNarguileDesc (String nomeNarguile);
+    List<NarguileEntity> findByNomeNarguileOrderByNomeNarguileDesc(String nomeNarguile);
 
-    List<NarguileEntity> findByMarcasNarguileOrderByMarcasNarguileDesc (MarcasNarguile marcasNarguile);
+    List<NarguileEntity> findByMarcasNarguileOrderByMarcasNarguileDesc(MarcasNarguile marcasNarguile);
 
-    List<NarguileEntity> findByQuantidadeMangueirasNarguileOrderByQuantidadeMangueirasNarguileDesc (Integer quantidadeMangueirasNarguile);
+    List<NarguileEntity> findByQuantidadeMangueirasNarguileOrderByQuantidadeMangueirasNarguileDesc(Integer quantidadeMangueirasNarguile);
 
-    List<NarguileEntity> findByMaterialNarguileOrderByMaterialNarguileDesc (MaterialNarguile materialNarguile);
+    List<NarguileEntity> findByMaterialNarguileOrderByMaterialNarguileDesc(MaterialNarguile materialNarguile);
 
     @Query(value = "SELECT * FROM NARGUILES n WHERE n.nome_narguile LIKE %?1%", nativeQuery = true)
-    List<NarguileEntity> procuraNomeNarguileComMetodoLike (String nomeNarguile);
+    List<NarguileEntity> procuraNomeNarguileComMetodoLike(String nomeNarguile);
 
     @Query(value = "SELECT * FROM NARGUILES n WHERE n.marcas_narguile LIKE %?1%", nativeQuery = true)
-    List<NarguileEntity> procuraMarcasNarguileComMetodoLike (String marcasNarguile);
+    List<NarguileEntity> procuraMarcasNarguileComMetodoLike(String marcasNarguile);
 
     @Query(value = "SELECT * FROM NARGUILES n WHERE n.material_narguile LIKE %?1%", nativeQuery = true)
-    List<NarguileEntity> procuraMaterialNarguileComMetodoLike (String materialNarguile);
+    List<NarguileEntity> procuraMaterialNarguileComMetodoLike(String materialNarguile);
 
-    List<NarguileEntity> findByQuantidadeMangueirasNarguileBetween (Integer quantidadeMinima, Integer quantidadeMaxima);
+    List<NarguileEntity> findByQuantidadeMangueirasNarguileBetween(Integer quantidadeMinima, Integer quantidadeMaxima);
 
-    List<NarguileEntity> findByQuantidadeEstoqueNarguileOrderByQuantidadeEstoqueNarguileDesc (Integer quantidadeEstoqueNarguile);
+    List<NarguileEntity> findByQuantidadeEstoqueNarguileOrderByQuantidadeEstoqueNarguileDesc(Integer quantidadeEstoqueNarguile);
 
-    void deleteByMarcasNarguile (MarcasNarguile marcasNarguile);
+    void deleteByMarcasNarguile(MarcasNarguile marcasNarguile);
 
-    void deleteByMaterialNarguile (MaterialNarguile materialNarguile);
+    void deleteByMaterialNarguile(MaterialNarguile materialNarguile);
 
-    void deleteByQuantidadeMangueirasNarguile (Integer quantidadeMangueirasNarguile);
+    void deleteByQuantidadeMangueirasNarguile(Integer quantidadeMangueirasNarguile);
 }
