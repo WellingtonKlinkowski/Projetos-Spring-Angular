391147444e46686da3d5b62c20b640c23ddcced4
WellingtonKlinkowski <wellimgtomklim@gmail.com>
2025-01-16 10:20:08 -0300

    Ajustes no DTO de Aluguel

--- manager-lounge/src/main/java/com/wklinkowski/manager_lounge/dtos/AluguelDTO.java
+++ manager-lounge/src/main/java/com/wklinkowski/manager_lounge/dtos/AluguelDTO.java
@@ -1,10 +1,11 @@
 package com.wklinkowski.manager_lounge.dtos;
 
 import com.wklinkowski.manager_lounge.entities.*;
-import jakarta.persistence.Column;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.BeanUtils;
 
 import java.time.Duration;
