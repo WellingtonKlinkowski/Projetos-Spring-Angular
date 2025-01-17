0b34c6c8846d82849f6866ec986441fac26fb6b6
WellingtonKlinkowski <wellimgtomklim@gmail.com>
2025-01-17 13:42:30 -0300

    Alterando a identação da exception para seguir um padrão

--- manager-lounge/src/main/java/com/wklinkowski/manager_lounge/exceptions/EntidadeNaoEncontrada.java
+++ manager-lounge/src/main/java/com/wklinkowski/manager_lounge/exceptions/EntidadeNaoEncontrada.java
@@ -3,6 +3,7 @@ package com.wklinkowski.manager_lounge.exceptions;
 import jakarta.persistence.EntityNotFoundException;
 
 public class EntidadeNaoEncontrada extends EntityNotFoundException {
+
     public EntidadeNaoEncontrada() {
         super("Não foi encontrado registro com esse identificador.");
     }
